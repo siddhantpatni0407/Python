2acb6f5f1db5a15cad22762b563b38ccf00c0af8
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2024-07-08 23:52:45 +0530

    Python Programs
    Python Programs

---
+++ com/sid/automatin/AutoWhatsAppMessage.py
@@ -0,0 +1,2 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+917276369171', 'Siya Patni here', 21, 40)
---
+++ com/sid/basics/listExample.py
@@ -0,0 +1,4 @@
+input_string = "apple,banana,cherry,date"
+values_list = input_string.split(',')
+print(values_list)
+print(type(values_list))
